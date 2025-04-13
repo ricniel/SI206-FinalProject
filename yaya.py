ae18db21b14c1eca2e6f65c4202ddbe28c0b3799
Yaya Harman <yaya@icloud.com>
2025-04-13 14:21:45 -0400

    completed scraping data into a dictionary

--- yaya.py
+++ yaya.py
@@ -11,6 +11,7 @@ import time
 
 # import necessary tools from the selenium library
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver import Safari
 
 # set up chrome driver
@@ -21,39 +22,51 @@ driver.get("https://www.iucnredlist.org/search/list?query=&searchType=species")
 
 time.sleep(5)
 
+
+# while True:
+#     try: 
+#         show_more = driver.find_element(By.CLASS_NAME, "section__link-out")
+
+#         show_more.click()
+
+#         time.sleep(2)
+#     except:
+#         print("No more 'Show More' button found or it's not clickable anymore.")
+#         break
+
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 species_card_list = soup.find_all('li', class_='list-results__item')
 
-species_dict = {}
+iucn_dict = {}
 
 for card in species_card_list:
-    sci_name = soup.find_all('p', class_="list-results__subtitle")
+    species_dict = {}
+    #print(f"card:{card}")
+    common_name = card.find('h2', class_='list-results__title').text.strip()
+    print(f'common name: {common_name}')
+
+    subtitle_tag = card.find_all('p', class_='list-results__subtitle')
+    for p_tag in subtitle_tag:
+        sci_name = p_tag.text.strip()
+        print(sci_name)
     
+    pop_status = card.find('span', class_='species-population').text.strip()
+    print(pop_status)
 
+    cat_tag = card.find('a', class_='species-category')
+    species_status = cat_tag['title']
+    print(species_status)
 
+    location = card.find('span', class_='species-assessment').text.strip()
+    location_list = location.split(', ')
+    print(location_list)
 
-# page = requests.get(url)
+    species_dict['Common Name'] = common_name
+    species_dict['Population Status'] = pop_status
+    species_dict['Red List Category'] = species_status
+    species_dict['Location'] = location_list
 
-# soup = BeautifulSoup(page.text, 'html.parser')
+    iucn_dict[sci_name] = species_dict
 
-
-# #get every species card 
-
-# def get_species_card():
-#     
-#     return species_card_list
-
-
-# #TEST CASES
-# class TestCases(unittest.TestCase):
-#     def test_species_card_list(self):
-#         self.assertEqual(type(get_species_card), list)
-
-
-# def main():
-#     get_species_card()
-
-# if __name__ == "__main__":
-#     unittest.main(verbosity=2) 
-    # main() 
+print(iucn_dict)
